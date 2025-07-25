9359d41aa8a71cfbb5378e3049da1b99908598fd
Ollie <Ollie_0@mail.ru>
2025-07-25 12:44:06 +0700

    added model Page for displaying all the pages in Administration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название страницы')
+    url = models.CharField(max_length=200, verbose_name='Адрес страницы')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Страница'
+        verbose_name_plural = 'Страницы'
+        ordering = ['title']
