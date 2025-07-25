d9b0493bf39ebb489a04b836f68d09daa08682d1
Ollie <Ollie_0@mail.ru>
2025-07-25 10:29:53 +0700

    connect templates to views and URLs

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('services/', views.services, name='services'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+    path('contacts/', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def portfolio(request):
+    return render(request, 'main/portfolio.html')
+
+def services(request):
+    return render(request, 'main/services.html')
+
+def testimonials(request):
+    return render(request, 'main/testimonials.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
