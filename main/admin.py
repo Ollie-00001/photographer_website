e759c1c2abfb32761290df5e7d3eef7c63842eca
Ollie <Ollie_0@mail.ru>
2025-07-25 13:20:50 +0700

    registered testimonial model

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Page
+from .models import Page, Testimonial
 
 # Register your models here.
 @admin.register(Page)
@@ -12,3 +12,17 @@ class PageAdmin(admin.ModelAdmin):
     def url_link(self, obj):
         return mark_safe(f'<a href="{obj.url}" target="_blank">{obj.url}</a>')
     url_link.short_description = 'Адрес страницы'
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'content', 'created_at')
+    search_fields = ('client_name', 'content')
+    ordering = ('-created_at',)
+
+    def __str__(self):
+        return f'{self.client_name}'
+    
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ['-created_at']
