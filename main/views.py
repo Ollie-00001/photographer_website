5f9c64a7ba820f9b5f96fcdc54e0f08cd0f9b578
Ollie <Ollie_0@mail.ru>
2025-07-25 12:46:14 +0700

    changed views for pages

--- main/views.py
+++ main/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
+from .models import Page
 
 def index(request):
-    return render(request, 'main/index.html')
+    pages = Page.objects.all()
+    return render(request, 'main/index.html', {'pages': pages})
 
 def portfolio(request):
-    return render(request, 'main/portfolio.html')
+    pages = Page.objects.all()
+    return render(request, 'main/portfolio.html', {'pages': pages})
 
 def services(request):
-    return render(request, 'main/services.html')
+    pages = Page.objects.all()
+    return render(request, 'main/services.html', {'pages': pages})
 
 def testimonials(request):
-    return render(request, 'main/testimonials.html')
+    pages = Page.objects.all()
+    return render(request, 'main/testimonials.html', {'pages': pages})
 
 def contacts(request):
-    return render(request, 'main/contacts.html')
+    pages = Page.objects.all()
+    return render(request, 'main/contacts.html',  {'pages': pages})
