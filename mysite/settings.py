d2f38f8fe283ba5236cb61cce1c777a0bc709420
Ollie <Ollie_0@mail.ru>
2025-07-25 13:21:39 +0700

    adjusted date/time format

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,14 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tomsk'
 
 USE_I18N = True
 
 USE_TZ = True
 
+DATE_FORMAT = 'd.m.Y'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
